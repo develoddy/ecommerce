fde6d42a3da4cefff9f69dc6ff3abac09f9cd9ce
Eddy Lujan <eddylujann@gmail.com>
2024-05-14 12:13:59 +0200

    add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,10 @@
 export const environment = {
     production: false,
-    URL_BACKEND: 'http://localhost:3000/',
-    URL_SERVICE: 'http://localhost:3000/api/',
-    URL_FRONTEND: 'http://localhost:5000/',
+    // URL_BACKEND: 'http://localhost:3000/',
+    // URL_SERVICE: 'http://localhost:3000/api/',
+    // URL_FRONTEND: 'http://localhost:5000/',
+
+    URL_BACKEND: 'https://api.lujandev.com/',
+    URL_SERVICE: 'https://api.lujandev/api/',
+    URL_FRONTEND: 'https://buy.lujandev.com/',
 };
