3beddbdbf0bee39c8d580e8ec88817e94800299a
Eddy Lujan <eddylujann@gmail.com>
2024-06-04 23:08:29 +0200

    fix enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
-    production: false,
+    production: true,
     // URL_BACKEND: 'http://localhost:3000/',
     // URL_SERVICE: 'http://localhost:3000/api/',
     // URL_FRONTEND: 'http://localhost:5000/',
 
     URL_BACKEND: 'https://api.lujandev.com/',
-    URL_SERVICE: 'https://api.lujandev/api/',
+    URL_SERVICE: 'https://api.lujandev.com/api/',
     URL_FRONTEND: 'https://tienda.lujandev.com/',
 };
