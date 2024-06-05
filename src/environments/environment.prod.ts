d429b23b43af955fd30b77bc34a6dd42d76b2a4a
Eddy Lujan <eddylujann@gmail.com>
2024-06-05 15:26:10 +0200

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,10 @@
 export const environment = {
     production: true,
-    
+
     URL_BACKEND: 'https://api.lujandev.com/',
     URL_SERVICE: 'https://api.lujandev.com/api/',
     URL_FRONTEND: 'https://tienda.lujandev.com/',
 };
+
+
+console.log('Production environment loaded', environment);
