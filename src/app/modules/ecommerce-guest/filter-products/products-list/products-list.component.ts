b4fe03a9e628060d748b9ab5ef5be4838c85de80
EVIDENZE\e.lujan <e.lujan@evidenze.com>
2025-09-02 23:38:47 +0200

    fix: toolbar

--- src/app/modules/ecommerce-guest/filter-products/products-list/products-list.component.ts
+++ src/app/modules/ecommerce-guest/filter-products/products-list/products-list.component.ts
@@ -14,13 +14,52 @@ export class ProductsListComponent implements OnInit, OnChanges {
   @Input() getRouterDiscount: any;
   @Input() changeProductImage: any;
 
+  pageSize = 12;
+  currentPage = 1;
+  pagedProducts: any[] = [];
+  totalPages = 1;
+
+  onPageSizeChange(event: Event) {
+    const target = event.target as HTMLSelectElement | null;
+    if (!target || !target.value) return; 
+    this.setPageSize(Number(target.value));
+  }
+
   ngOnInit() {
-    console.log('[ProductsListComponent][ngOnInit] products:', this.products);
+    this.updatePagedProducts();
   }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes.products) {
-      console.log('[ProductsListComponent][ngOnChanges] products:', this.products);
+      this.currentPage = 1;
+      this.updatePagedProducts();
     }
+  }
+
+  updatePagedProducts() {
+    this.totalPages = Math.ceil(this.products.length / this.pageSize) || 1;
+    const start = (this.currentPage - 1) * this.pageSize;
+    const end = start + this.pageSize;
+    this.pagedProducts = this.products.slice(start, end);
+  }
+
+  goToPage(page: number) {
+    if (page < 1 || page > this.totalPages) return;
+    this.currentPage = page;
+    this.updatePagedProducts();
+  }
+
+  nextPage() {
+    this.goToPage(this.currentPage + 1);
+  }
+
+  prevPage() {
+    this.goToPage(this.currentPage - 1);
+  }
+
+  setPageSize(size: number) {
+    this.pageSize = +size;
+    this.currentPage = 1;
+    this.updatePagedProducts();
   }
 }
