5c3882c60632c35487b73a101c716295496777e6
Eddy Lujan <eddylujann@gmail.com>
2026-01-04 17:57:07 +0100

    💾 Pre-Deploy commit 2026-01-04 17:57:07

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,10 +4,9 @@ export const environment = {
     URL_BACKEND: 'https://api.lujandev.com/',
     URL_SERVICE: 'https://api.lujandev.com/api/',
     URL_FRONTEND: 'https://tienda.lujandev.com/',
-    stripePublicKey: 'pk_test_51RkV1QCtfffoVXXrTORpzxQKnNHLvLX09uwlHfyPB242r5UY2VWSI1TxO1AUPxeADN6A6Xghp2EC1y8ijSD6sKeg00qO0iW7Ab',
-    // ⚠️ PayPal: Cambiar a Client ID de PRODUCCIÓN cuando estés listo
-    // Por ahora usa sandbox para testing seguro en producción
-    paypalClientId: 'AXuACmEIWqiMwegE6pisIhiTOPldHac-2XYD0aJjoPuy34JB6grxNeDc-1jgTqv_W5QvC7o4pZIvVe8G',
+    stripePublicKey: 'pk_live_51PtUUBChg0ofSaFLNlrWqGCFgjdIshjSZNgx8QHEyJw6iXayFEeRog9yA74eSplm67AmDdt4Li2qsU0trAjcdkNo00lETaDpUZ',
+    // ✅ PayPal: Credenciales de PRODUCCIÓN (Live Mode)
+    paypalClientId: 'BAAReFy_dQiYJW2O80Uf_ZMZKDSb-EAJsyQeTjC6BN2PFxFpfh_hi-mAG69DGjHmTs2K-ghBGfIH7ehmVU',
     sentryDsn: 'https://541cd1a333fb1d2682bbd12a394ac3e1@o4510481830707200.ingest.de.sentry.io/4510481837391952' // 👈 Reemplazar con tu DSN real de producción
 };
 
